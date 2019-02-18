7c1be974c33b64011b40f0d1338a8405183ec746
shapiroj <joshua.shapiro@gmail.com>
2019-02-18 04:29:27 +0000

    Changed SMS notification

--- app/commands/send_sms_notification.rb
+++ app/commands/send_sms_notification.rb
@@ -4,13 +4,14 @@ class SendSmsNotification
     prepend SimpleCommand
     
     def initialize(pass)
-        @sender = pass.purchaser.phone_number.to_s
+        @product = pass.buyable.name.downcase
+        @sender = pass.purchaser.name.to_s
         @account = pass.account
     end
     
     def call
         
-      message = "Hi.  You've got a drink waiting for you at TooU courtesy of #{@sender}.  Download the app at https://toou.gifts to get your drink."
+      message = "Hi.You've got a #{@product} waiting for you at TooU courtesy of #{@sender}.  Visit https://toou.gifts to get your drink."
       phone = @account.phone_number.to_s
       if MessageSender.new.send_message(phone, message)
         Log.create(log_type: Log::INFO, context: SendSmsNotification.name, current_user: @account.id, message: "SMS sent to #{phone}")
