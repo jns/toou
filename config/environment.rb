8f2dff3e2e85b792934728d2ab36d736d761cb26
shapiroj <joshua.shapiro@gmail.com>
2019-03-31 17:19:06 +0000

    Updated action mailer for mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ Rails.application.initialize!
 
 # Configure the mailer
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'toou.gifts',
-  :address => 'smtp.sendgrid.net',
-  :port => 465,
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => ENV['MAILGUN_DOMAIN'],
   :authentication => :plain,
-  :enable_starttls_auto => true
 }
+ActionMailer::Base.delivery_method = :smtp
