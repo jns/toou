78064e8ac8dce6efbe79c63199afb79b6f0b9345
Joshua Shapiro <josh@toou.gifts>
2019-08-17 01:52:41 +0000

    Added merchant map

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get 'pass', to: 'welcome#pass_not_found'
   get 'support', to: 'welcome#support'
   get "faq", to: 'welcome#faq'
+  get "merchant_map", to: 'welcome#merchant_map'
   
   # Redemption 
   get 'mredeem', to: 'redemption#index'
