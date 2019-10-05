71a312bc2e36938cdf24621315e642c605d71644
Joshua Shapiro <josh@toou.gifts>
2019-10-05 05:09:03 +0000

    Updated message in PassRedeemedNotificationJob to include merchant

--- app/jobs/pass_redeemed_notification_job.rb
+++ app/jobs/pass_redeemed_notification_job.rb
@@ -9,7 +9,7 @@ class PassRedeemedNotificationJob < ApplicationJob
       recipient = pass.recipient
       recipient_name = (recipient.name ? recipient.name : recipient.phone_number)
       sender = pass.purchaser
-      message = "Hooray! #{recipient_name.to_s} just used the TooU you sent them for a #{product}"
+      message = "🍻Hooray! #{recipient_name.to_s} just used the TooU you sent them for a #{product} at #{pass.merchant.name}"
       if acct.can_receive_notifications?
           SendDeviceNotification.call(sender, "TooU Redeemed", message) unless sender.test_user?
       else
