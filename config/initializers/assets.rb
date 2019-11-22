9bf1f9c3732341919a57aa511319df9436aa251f
Joshua Shapiro <josh@toou.gifts>
2019-11-22 00:31:49 +0000

    Changed asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( open-iconic.min.svg )
+Rails.application.config.assets.precompile += %w( splash.js )
 Rails.application.config.assets.precompile += %w( welcome.js )
 Rails.application.config.assets.precompile += %w( merchants.js )
 Rails.application.config.assets.precompile += %w( admin.js )
