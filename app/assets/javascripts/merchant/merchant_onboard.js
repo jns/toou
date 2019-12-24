cb0ea21843473c5f6f02c902f1950627ca59069a
Joshua Shapiro <josh@toou.gifts>
2019-12-24 23:53:19 +0000

    onboarding layout

--- app/assets/javascripts/merchant/merchant_onboard.js
+++ app/assets/javascripts/merchant/merchant_onboard.js
@@ -7,11 +7,13 @@ var MerchantAddressForm = (function() {
         var merchant_name = "";
         var formatted_addr = "";
         var international_phone = "";
+        var website = "https://";
         
         if (vnode.attrs.place) {
             merchant_name = vnode.attrs.place.name;
             formatted_addr = vnode.attrs.place.formatted_address;
             international_phone = vnode.attrs.place.international_phone_number;
+            website = vnode.attrs.place.website;
         }
         
         return m("form", [
@@ -22,6 +24,8 @@ var MerchantAddressForm = (function() {
                 m("input.form-control", {type: "text", name: "merchant_address", value: formatted_addr}),
                 m("label", {for: "merchant_phone"}, "Phone"),
                 m("input.form-control", {type: "text", name: "merchant_phone", value: international_phone}),
+                m("label", {for: "merchant_website"}, "Website"),
+                m("input.form-control", {type: "text", name: "merchant_website", value: website}),
                 ]),
             m(".form-group"),
             ]
--- app/assets/javascripts/modal2.js
+++ app/assets/javascripts/modal2.js
@@ -8,6 +8,18 @@ var Modal2 = function(components) {
     
     var data = {};
 
+    var oncreate = function(vnode) {
+        var wizItemsHeight = "0px";
+        $(".wiz-item").each(function() {
+           var item = $(this);
+           var h = item.css("height");
+           if (h > wizItemsHeight) { 
+                wizItemsHeight = h;   
+           }
+        });
+        $(".wiz-items").css("height", wizItemsHeight);
+    }
+
     var advance = function(ev) {
         var active = components.find(function(c) { return c.state == "active";});
         var next = components.find(function(c) { return c.state == "staged"; });
@@ -18,13 +30,13 @@ var Modal2 = function(components) {
 
     var view = function(vnode) {
         var comps = components.map(function(c) { return m(".wiz-item."+c.state, m(c, data))});
-        return m(".wiz", [comps, 
+        return m(".wiz", [m(".wiz-items.clearfix", comps), 
                          m(".wiz-control", [
                              m("input.btn.btn-light.text-right", {type: "button", onclick: advance, value: "Next"}),
                              ])]);
     };
     
     
-    return {view: view};
+    return {view: view, oncreate: oncreate};
     
 };
